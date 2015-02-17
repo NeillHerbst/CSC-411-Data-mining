5453e8441175dd2b526ac3c48c5a34e9ece22f18
Neill Herbst <n.herbst6@gmail.com>
2015-02-17 23:04:05 +0200

    Added method for reading headers from file. Adjusted rest of code accordingly.

--- TGA_reader.py
+++ TGA_reader.py
@@ -1,27 +1,64 @@
 import pandas as pd
 import os
 import json
+import io
 
-with open('config.json') as f:
+
+with open('config.json', 'rU') as f:
     config = json.load(f)
 
-txtfile = os.path.join(config['datadir2'],'Sample 74 in air.txt')
-details ={}
-datafile = open(txtfile,'rU')
+txtfile = os.path.join(config['datadir2'], 'Sample 74 in air.txt')
+datafile = io.open(txtfile, 'rU', encoding='iso8859')
+
+details = {}
+datapoints = []
+read_names = []
+names = ['']
+
+# Collecting metadata
 
 for line in datafile:
-    if "0.000000" in line:
-        break
     lineitems = line.strip().split('\t')
-    
-    if len(lineitems)==2:
+
+    if len(lineitems) == 2:
         details[lineitems[0]] = lineitems[1]
     else:
-            details[lineitems[0]] = lineitems[1:]
-            
-names = ['Experiment','Time', 'Unsub_weight', 'Base_weight','Prog_Temp','Samp_Temp','G_Flow','Diag_Temp']
+        details[lineitems[0]] = lineitems[1:]
 
+    if "TGA Isothermal" in line:
+        break
 
-datafile = pd.read_csv(txtfile,delimiter='\t',skiprows=33)
+# Collecting header names
+
+for i, line in enumerate(datafile):
+
+    if 'Time' in line:
+        lineitems = line.strip().split('\t')
+        for i in range(len(lineitems)):
+            read_names.append(lineitems[i])
+
+    if 'Gas Flow' in line:
+        lineitems = line.strip().split('\t')
+        for i in range(len(lineitems)):
+            read_names.append(lineitems[i])
+
+    if '0.000000' in line:
+        break
+
+for i in range(7):
+    if i == 0:
+        names.append(read_names[0].rstrip())
+    else:
+        names.append(read_names[i].rstrip() + ' ' + read_names[i + 6].rstrip())
+
+# Reading data and populating DataFrame
+datafile.close()
+datafile = io.open(txtfile, 'rU', encoding='iso8859')
+
+for i, line in enumerate(datafile):
+    if 'Gas Flow' in line:
+        break
+
+datafile = pd.read_csv(datafile, delimiter='\t', header=None)
 dataframe = pd.DataFrame(datafile)
 dataframe.columns = names
