9d31af5a64bed96318611f51fd888e986e9c0317
Neill Herbst <n.herbst6@gmail.com>
2015-03-19 22:45:57 +0200

    added foreign keys

--- Relationships.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Mar 19 16:26:44 2015
-
-@author: Neill
-"""
-
-import sqlite3 as lite
-con = lite.connect(':memory: test.db')
--- access_db.py
+++ access_db.py
@@ -34,6 +34,7 @@ if stop is False:
         parm_type = datafile.parse('Parm type', header=0)
         parameters = datafile.parse('Parameters', header=0)
         values = datafile.parse('Parm values', header=0)
+        steps = datafile.parse('Steps', header=0)
 
         people_df = pd.DataFrame(people)
         batches_df = pd.DataFrame(batches)
@@ -41,6 +42,7 @@ if stop is False:
         parm_type_df = pd.DataFrame(parm_type)
         parameters_df = pd.DataFrame(parameters)
         values_df = pd.DataFrame(values)
+        steps_df = pd.DataFrame(steps)
 
         people_df.to_sql('People1', con, if_exists='replace')
         batches_df.to_sql('Batches1', con, if_exists='replace')
@@ -48,32 +50,53 @@ if stop is False:
         parm_type_df.to_sql('Parm_type1', con, if_exists='replace')
         parameters_df.to_sql('Parameters1', con, if_exists='replace')
         values_df.to_sql('Parm_values1', con, if_exists='replace')
+        steps_df.to_sql('Steps1', con, if_exists='replace')
 
         cur.executescript('''
+
         drop table if exists Parm_values;
         drop table if exists People;
         drop table if exists Batches;
         drop table if exists Synthesis;
         drop table if exists Parm_type;
         drop table if exists Parameters;
+        drop table if exists Steps;
 
-        create table Parm_values(batch_id INT, syn_id INT, step_id INT, \
-        par_id INT, val);
-        create table People(owner_id INT PRIMARY KEY, description TEXT);
-        create table Batches(batch_id TEXT PRIMARY KEY, syn_id INT, owner_id);
         create table Synthesis(syn_id INT PRIMARY KEY, description TEXT);
         create table Parm_type(type_id INT PRIMARY KEY, type TEXT);
+
         create table Parameters(par_id INT PRIMARY KEY, type_id INT, \
-        description TEXT);
+        description TEXT, FOREIGN KEY (type_id) REFERENCES Parm_type(type_id));
+
+        create table People(owner_id INT PRIMARY KEY, description TEXT);
+
+        create table Batches(batch_id TEXT PRIMARY KEY, syn_id INT, owner_id,\
+            FOREIGN KEY (syn_id) REFERENCES Synthesis(syn_id),\
+            FOREIGN KEY (owner_id) REFERENCES People(owner_id));
+
+        create table Steps(syn_id INT, step_id INT PRIMARY KEY, par_id INT,\
+            sequence INT, description TEXT,\
+            FOREIGN KEY (syn_id) REFERENCES Synthesis(syn_id),\
+            FOREIGN KEY (par_id) REFERENCES Parameters(par_id));
+
+        create table Parm_values(batch_id INT, syn_id INT, step_id INT, \
+            par_id INT, val,\
+            FOREIGN KEY (batch_id) REFERENCES Batches(batch_id),\
+            FOREIGN KEY (syn_id) REFERENCES Synthesis(syn_id),\
+            FOREIGN KEY (step_id) REFERENCES Steps(step_id),\
+            FOREIGN KEY (par_id) REFERENCES Parameters(par_id));
 
         insert into Parm_values select batch_id, syn_id, step_id, par_id, val \
-        from Parm_values1;
+            from Parm_values1;
+
         insert into People select owner_id, description from People1;
         insert into Batches select batch_id, syn_id, owner_id from Batches1;
         insert into Synthesis select syn_id, description from Synthesis1;
         insert into Parm_type select type_id, type from Parm_type1;
         insert into Parameters select par_id, type_id, description from \
-        Parameters1;
+            Parameters1;
+        insert into Steps select syn_id, step_id, par_id, sequence, \
+            description from Steps1;
 
         drop table Parm_values1;
         drop table People1;
@@ -81,4 +104,5 @@ if stop is False:
         drop table Synthesis1;
         drop table Parm_type1;
         drop table Parameters1;
+        drop table Steps1
         ''')
