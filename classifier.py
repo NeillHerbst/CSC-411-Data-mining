bdc1ce4cc5783ff4f7a070a9af11a751668b9b4d
Neill Herbst <n.herbst6@gmail.com>
2015-05-13 19:37:53 +0200

    added code for identifying elements in samples.

--- classifier.py
+++ classifier.py
@@ -10,6 +10,7 @@ import pandas as pd
 import json
 import os
 import numpy as np
+import re
 
 # Retrieve all working directories
 with open('config.json') as f:
@@ -24,9 +25,36 @@ def data_filter(DataFrame, colums):
     df = DataFrame
     df = df[collist]
     for col in colums:
-        df = df[np.isfinite(df[col])]
+        if col != 'Elements / Ratio':
+            df = df[np.isfinite(df[col])]
     df = df.reset_index(drop=True)
     return df
+
+
+def component(DataFrame, column):
+    df = DataFrame
+    vals = df[column].values
+    Npoints = len(vals)
+    ca_lst = np.zeros(Npoints)
+    mg_lst = np.zeros(Npoints)
+
+    matcher = re.compile(r"(Ca\b|Ca\d|Ca[^O,o]|Cal[M,m]|Calcium|Lime|Katoite|\
+                         kat|[H, h]ydrocalumite|[P, p]ortlanite|[D,d]olomite)\
+                         |(Mg\b|Mg\d|\Mg[^O,o]|Mag\b|Magnesium|Bricite\
+                         |[M, m]eixnerite|HTC|[H, h]ydrotalcite|Dolomite\
+                         |Pyrosorb|Alcimazer|Meix)|([C, c]alcined)")
+
+    for i, line in enumerate(vals):
+        m = matcher.match(line.strip())
+        if m:
+            ca, mg, calcined = m.groups()
+            if ca and not calcined:
+                ca_lst[i] = 1
+
+            if mg:
+                mg_lst[i] = 1
+
+    return ca_lst, mg_lst
 
 # Data path
 Dat_path = filename('Data', 'Sample_list_v2.0.xlsx')
@@ -39,19 +67,26 @@ Dat_file = Dat_file.parse('Sample List')
 Df = pd.DataFrame(Dat_file)
 
 # Colums used for SVM
-collist = ['Stirrer Time', 'Temp (C)', 'Result']
+collist = ['Elements / Ratio', 'Stirrer Time', 'Temp (C)', 'Result']
 
 # Filtering data to remove rows containing empty values
 Df = data_filter(Df, collist)
 
+# Lists of results for samples containing Ca an Mg
+ca_lst, mg_lst = component(Df, 'Elements / Ratio')
+
+# Adding List of Ca and Mg results to Dataframe
+Df['Ca'] = ca_lst
+Df['Mg'] = mg_lst
+
 # Fraction of data for training
-frac = 0.5
+frac = 0.8
 
 # Calculating Dataframe split position
 split = len(Df) * frac
 
 # Data for SVM
-x_lst = ['Stirrer Time', 'Temp (C)']
+x_lst = ['Stirrer Time', 'Temp (C)', 'Ca', 'Mg']
 X = Df[x_lst]
 
 x_train = X[X.index <= split]
