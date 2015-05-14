8ce677f25d281e67d8b5a2c9c568d676c605f840
Neill Herbst <n.herbst6@gmail.com>
2015-05-14 17:23:39 +0200

    added code for printing all methods

--- plot_ascii.py
+++ plot_ascii.py
@@ -16,7 +16,7 @@ import heapq
 import pandas as pd
 
 TWOTHETA_CUTOFF = 5
-METHOD = 'Intercalation'
+METHOD = 'all'
 
 # Get all working directories
 with open('config.json') as f:
@@ -39,6 +39,7 @@ def readpeaks(peakfile):
                           if val in maxpeaks[:3]])
     return name, theta, intens
 
+
 def method_filter(method):
 
     # Retrieving files
@@ -51,16 +52,44 @@ def method_filter(method):
     batch_id = xl_batches['batch_id'][:].values
     syn_id = xl_batches['syn_id'][:].values
     syn_names = xl_syn_names['description'][:].values
+    syn_names_id = xl_syn_names['syn_id'][:].values
 
-    # retrieving method id as displayed in excel file
-    m_id = np.where(syn_names == method)[0][0] + 1
+    if method == 'all':
+        return batch_id, syn_id, syn_names, syn_names_id
+
+    elif method != 'all':
+        # retrieving method id as displayed in excel file
+        m_id = np.where(syn_names == method)[0][0] + 1
+
+        # filtering values
+        batch_index = np.where(syn_id != m_id)
+        batch_id = np.delete(batch_id, batch_index)
+        syn_id = np.delete(syn_id, batch_index)
+
+        return batch_id, syn_id, syn_names, syn_names_id
 
 
-    # filtering values
-    batch_index = np.where(syn_id != m_id)
-    batch_id = np.delete(batch_id, batch_index)
+def method_name(batch_id, batch_file, METHOD):
+    if batch_file in batch_id:
+        method_found = True
+        if METHOD == 'all':
+            syn_index = np.where(batch_id == batch_file)[0][0]
+            syn_id = syn_ids[syn_index]
 
-    return batch_id
+            if np.isfinite(syn_id):
+                name_id = np.where(syn_names_id == syn_id)[0][0]
+                syn_name = syn_names[name_id]
+
+            else:
+                method_found = False
+                syn_name = None
+        else:
+            syn_name = METHOD
+    else:
+            method_found = False
+            syn_name = None
+
+    return syn_name, method_found
 
 # Initialize the counter
 n = 0
@@ -79,7 +108,7 @@ output_path = filename('Data', 'XRD_results.csv')
 filename_lst = []
 
 # filtering methods from sample list
-batch_id = method_filter(METHOD)
+batch_id, syn_ids, syn_names, syn_names_id = method_filter(METHOD)
 k = 0
 matcher = re.compile('.*[_ ]([0-9]+)([a-z]?).*')
 # Loop through all ASCII files
@@ -95,60 +124,60 @@ with PdfPages(os.path.join(plot_path, 'All plots.pdf')) as pdf:
         # batch number from XRD file name
         batch_file = str(number).zfill(3) + subnumber
 
-        # Comparing XRD file number with sample list number
-        method_found = batch_file in batch_id
-
-        if not subnumber:
-            subnumber = 'a'
-        plot_name = 'XRD_{0:04d}.{1}'.format(int(number), subnumber)
-
-        # Check if plot already exists
-        plot_filename = os.path.join(plot_path, '{0}.pdf'.format(plot_name))
-        exists = os.path.isfile(plot_filename)
-
-        # Plot non-existing files
-        if not exists:
-            # Save filename to record
-            filename_lst.append(plot_name)
-
-            # Counter
-            n += 1
-
-            # Load data from file
-            x, y = np.loadtxt(file_path, usecols=(0, 1), unpack=True)
-
-            # Filter for 2theta cutoff
-            good = x >= TWOTHETA_CUTOFF
-            x = x[good]
-            y = y[good]
-
-            # Plot data
-            fig = plt.figure()
-            plt.plot(x, y, 'k')
-
-            # Plot peaks
-            for linefmt, (name, theta, intens) in zip(linefmts, peakpatterns):
-                # Get actual count values
-                peak = np.interp(theta[0], x, y)
-                plt_peak = np.array(intens)/100.*peak
-                plt.stem(theta, plt_peak,
-                         linefmt=linefmt, markerfmt=' ', basefmt=' ',
-                         label=name)
-
-            plt.xlabel(r"$2\theta$")
-            plt.ylabel('Counts')
-            plt.title(plot_name)
-            plt.legend(loc=0)
-
-            if method_found:
-                plt.figtext(0.73, 0.68, METHOD, weight='bold')
-
-            plt.savefig(plot_filename)
-            pdf.savefig()
-            plt.close()
-print '{0} Files plotted'.format(n)
+        # Comparing XRD file number with sample list number and retrieving method name
+        syn_name, method_found = method_name(batch_id, batch_file, METHOD)
+        
+#        if not subnumber:
+#            subnumber = 'a'
+#        plot_name = 'XRD_{0:04d}.{1}'.format(int(number), subnumber)
+#
+#        # Check if plot already exists
+#        plot_filename = os.path.join(plot_path, '{0}.pdf'.format(plot_name))
+#        exists = os.path.isfile(plot_filename)
+#
+#        # Plot non-existing files
+#        if exists:
+#            # Save filename to record
+#            filename_lst.append(plot_name)
+#
+#            # Counter
+#            n += 1
+#
+#            # Load data from file
+#            x, y = np.loadtxt(file_path, usecols=(0, 1), unpack=True)
+#
+#            # Filter for 2theta cutoff
+#            good = x >= TWOTHETA_CUTOFF
+#            x = x[good]
+#            y = y[good]
+#
+#            # Plot data
+#            fig = plt.figure()
+#            plt.plot(x, y, 'k')
+#
+#            # Plot peaks
+#            for linefmt, (name, theta, intens) in zip(linefmts, peakpatterns):
+#                # Get actual count values
+#                peak = np.interp(theta[0], x, y)
+#                plt_peak = np.array(intens)/100.*peak
+#                plt.stem(theta, plt_peak,
+#                         linefmt=linefmt, markerfmt=' ', basefmt=' ',
+#                         label=name)
+#
+#            plt.xlabel(r"$2\theta$")
+#            plt.ylabel('Counts')
+#            plt.title(plot_name)
+#            plt.legend(loc=0)
+#
+#            if method_found:
+#                plt.figtext(0.73, 0.68, syn_name, weight='bold')
+#
+##            plt.savefig(plot_filename)
+##            pdf.savefig()
+#            plt.close()
+#print '{0} Files plotted'.format(n)
 
 # Save file
-output_dict = {'Result': [np.NaN]*len(filename_lst), 'Sample No': filename_lst}
-output_df = pd.DataFrame(output_dict)
-output_df.to_csv(output_path)
+#output_dict = {'Result': [np.NaN]*len(filename_lst), 'Sample No': filename_lst}
+#output_df = pd.DataFrame(output_dict)
+#output_df.to_csv(output_path)
