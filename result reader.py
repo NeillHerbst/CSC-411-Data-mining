c7a613ca7650e021bfbb283736ce7e1e2e97c743
Neill Herbst <n.herbst6@gmail.com>
2015-05-16 19:48:59 +0200

    fixed code, now retrieves results properly

--- result reader.py
+++ result reader.py
@@ -20,41 +20,41 @@ with open('config.json') as f:
 
 def filename(location, pattern):
     return os.path.expanduser(os.path.join(config[location], pattern))
+
+data_path = filename('Data', 'Sample_list_v3.0.xlsx')
+data = pd.ExcelFile(data_path).parse('Sample List')
+sample_no = pd.DataFrame(data)['Item Name'].values
+
+
+def result_finder(Sample_numbers, Results_file):
+    results = filename('XRD Results', Results_file)
+    results_file = pd.read_csv(results, sep=';')[['Result', 'Sample No']]
+    XRD_results = results_file['Result']
+    XRD_no = results_file['Sample No']
+    matcher = re.compile(r'([0-9]+).([a-z]?)')
+    matcher1 = re.compile('([0-9]+)([a-z]?)')
+    results_lst = [np.NaN]*len(Sample_numbers)
     
-# Results path
-results = filename('XRD Results', 'XRD_Results.csv')
-Dat_path = filename('Data', 'Sample_list_v2.0.xlsx')
+    for i, s_num in enumerate(Sample_numbers):
+        m1 = matcher1.match(s_num.strip())
 
-Data = pd.ExcelFile(Dat_path).parse('Sample List')
-sample_lst_no = pd.DataFrame(Data)[r'Item Name']
-results_file = pd.read_csv(results, sep=';')[['Result', 'Sample No']]
-XRD_results = results_file['Result']
-XRD_no = results_file['Sample No']
+        if m1:
+            num1, subnum1 = m1.groups()
+            if not subnum1:
+                num1 = '{:04d}.a'.format(int(num1))
 
+            elif subnum1:
+                    num1 = '{:04d}.{}'.format(int(num1), subnum1)
 
-matcher = re.compile(r'([0-9]+)[.]([a-z]?)')
-matcher1 = re.compile('([0-9]+)([a-z]?)')
+            for j, xrd_num in enumerate(XRD_no):
+                m2 = matcher.search(xrd_num.strip())
 
-results_init = np.zeros(len(sample_lst_no))
+                if m2:
+                    num2, subnum2 = m2.groups()
+                    num2 = '{:04d}.{}'.format(int(num2), subnum2)
 
-for i, s_num in enumerate(sample_lst_no):
-
-    m1 = matcher1.match(s_num)
-    if m1:
-        num1, subnum1 =  m1.groups()
-        if not subnum1:
-            num1 = '{:04d}.a'.format(int(num1))
-        
-        elif subnum1:
-                num1 = '{:04d}.{}'.format(int(num1), subnum1)
-#        print num1   
-        for j, xrd_num in enumerate(XRD_no):
-#            print s_num, xrd_num
-            m2 = matcher.match(xrd_num)
-            print m2, xrd_num
-            if m2:
-                num2, subnum2 = m2.groups()
-                num2 = '{}.{}'.format(int(num2), subnum2)
-                print num2
-                if num1 == num2:
-                    results_init[i] = XRD_results[j]
+                    if num1 == num2:
+                        results_lst[i] = XRD_results[j]
+    return results_lst
+results = result_finder(sample_no, 'XRD_Results_old.csv')
+print results
